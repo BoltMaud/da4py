481563f2988aa930c2fc4e6542f1c44d228646c6
Mathilde BOLTENHAGEN <mathilde.boltenhagen@lsv.fr>
2019-09-12 10:41:39 +0200

    progession

--- darksider4py/pn_to_formulas.py
+++ darksider4py/pn_to_formulas.py
@@ -1,51 +1,82 @@
 from darksider4py.formulas import Or, And
 import sys
+
+from darksider4py import variablesGenerator as vg
+
 sys.path.append('../pm4py-source')
 from pm4py.objects.petri import importer
+from pm4py.objects.petri.common import final_marking, initial_marking
 
-def is_run(n, pn, m_ip,lambda_ia,sigma):
-    positives = [m_ip([0,m]) for m in pn.initialMarking]
-    negatives = [m_ip([0,m]) for m in (pn.places-pn.initialMarking) ]
-    formulas = [is_action(pn,i,m_ip,lambda_ia,sigma) for i in range(1,n+1)]
+
+def is_run(size_of_run, places, transitions, m0, m_ip,tau_it):
+    positives = [m_ip([0,places.index(m)]) for m in m0]
+    negatives = [m_ip([0,places.index(m)]) for m in places if m not in m0]
+    formulas = [is_action(places, transitions, m0,i,m_ip,tau_it) for i in range(1,size_of_run)]
     run_of_pn = And(positives,negatives,formulas)
     return  run_of_pn
 
-def is_action(pn, i, m_ip, lambda_ia, sigma):
+def is_action(places, transitions, m0, i, m_ip, tau_it):
     '''
     :param pn:
     :param i:
     :param m_ip:
-    :param lambda_ia:
+    :param tau_it:
     :param sigma:
     :return: give action of instant i
     '''
-
-    #only one action is true at instant i
-    formulas= [Or([lambda_ia([i,a] for a in sigma)],[],[])]
-    # if we have the action a, then we have the good marking and it fires
-    for a in sigma:
-        one_is_transitions=[]
-        for transition in pn.transitions.items():
-            if transition.label==a:
-                one_is_transitions.append(is_transition(pn,transition,i,m_ip))
-        formulas.append(Or([],[lambda_ia([i,a])],one_is_transitions))
+    #only one transition is true at instant i
+    formulas= [Or([tau_it([i,transitions.index(t)]) for t in transitions],[],[])]
+    # if transition t fires at instant i, then we have the good marking
+    for t in transitions:
+        formulas.append(Or([],[tau_it([i,transitions.index(t)])],is_transition(places,t,i,m_ip)))
     return And([],[],formulas)
 
-def is_transition(pn,transition,i,m_ip):
+def is_transition(places,transition,i,m_ip):
     formulas=[]
-    for place in pn.places.items():
-        if place.id in transition.prePlaces and place.id in transition.postPlaces:
-            formulas.append(And([m_ip([i,place.id]),m_ip([i-1,place.id])],[],[]))
-        elif place.id in transition.prePlaces and place.id not in transition.postPlaces:
-            formulas.append(And([m_ip([i-1,place.id])],[m_ip([i,place.id])],[]))
-        elif place.id not in transition.prePlaces and place.id in transition.postPlaces:
-            formulas.append(And([m_ip([i,place.id])],[m_ip([i-1,place.id])],[]))
+    prePlaces=[ a.source for a in transition.in_arcs ]
+    postPlaces=[a.target for a in transition.out_arcs]
+
+    for p in places:
+        if p in prePlaces and p in postPlaces:
+            formulas.append(And([m_ip([i,places.index(p)]),m_ip([i-1,places.index(p)])],[],[]))
+        elif p in prePlaces and p not in postPlaces:
+            formulas.append(And([m_ip([i-1,places.index(p)])],[m_ip([i,places.index(p)])],[]))
+        elif p not in prePlaces and p in postPlaces:
+            formulas.append(And([m_ip([i,places.index(p)])],[m_ip([i-1,places.index(p)])],[]))
         else :
-            formulas.append(Or([],[],[And([m_ip([i,place.id]),m_ip([i-1,place.id])],[],[]),
-                                      And([],[m_ip([i,place.id]),m_ip([i-1,place.id])],[])]))
+            formulas.append(Or([],[],[And([m_ip([i,places.index(p)]),m_ip([i-1,places.index(p)])],[],[]),
+                                      And([],[m_ip([i,places.index(p)]),m_ip([i-1,places.index(p)])],[])]))
     And([],[],formulas)
+
+def petri_net_to_SAT(net, m0, mf, variablesGenerator, size_of_run, label_m="m_ip", label_t="tau_it"):
+    '''
+    This function returns the SAT formulas of a petrinet given label of variables, size_of_run
+    :param net: petri net of the librairie pm4py
+    :param m0: initial marking
+    :param mf: final marking
+    :param variablesgenerator: @see darksider4py.variablesGenerator
+    :param label_m (string) : name of marking boolean variables per instant i and place p
+    :param label_t (string) : name of place boolean variables per instant i and transition t
+    :param size_of_run (int) : max instant i
+    :param sigma (list of char) : transition name
+    :return: a boolean formulas
+    '''
+    # we need a ordered list to get int per place/transition (for the variablesgenerator)
+    transitions=[t for t in net.transitions]
+    places=[p for p in net.places]
+
+    # we create the number of variables needed for the markings
+    variablesGenerator.add(label_m, [(0,size_of_run),(0,len(places)-1)])
+
+    # we create the number of variables needed for the transitions
+    variablesGenerator.add(label_t, [(0,size_of_run-1),(0,len(transitions)-1)])
+    return is_run(size_of_run, places, transitions,m0,variablesGenerator.get(label_m),variablesGenerator.get(label_t))
 
 
 net, m0, mf = importer.pnml.import_net("../examples/example.pnml")
-print(m0)
+variables=vg.variablesGenerator()
+petri_net_to_SAT(net, m0,mf,variables,10)
+
+
+
 
--- darksider4py/variablesGenerator.py
+++ darksider4py/variablesGenerator.py
@@ -1,29 +1,55 @@
 
-class setOfVariables:
+class variablesGenerator:
+    '''
+    this class creates an integer per variable
+    example : we need 5 "m" variables m1, m2, m3, m4, m5 then we will get 1 2 3 4 5
+    '''
     def __init__(self):
         self.set={}
         self.iterator=0
 
     def add(self, name, listOfCouples):
+        '''
+        add a type of variables from p1 to p2
+        :param name (string) : name of the var
+        :param listOfCouples (int, int) : bounds of the var
+        :return (void) create a key "name" with the numbers of the vars
+
+        for example :
+        test=variablesGenerator()
+        test.add("m",[(0,4),(0,5),(0,8)])
+        we have 4*5*8 m variables
+        '''
         base=self.iterator
         def newfunction(listOfElements):
+            '''
+            a subfunction per variable that computes an element
+            :param listOfElements (indexes) : in the previous example it could be [2,3,4]
+            :return the value of those indexes (int)
+            '''
             result=base
+            #for each index
             for i in range (0,len(listOfElements)):
+                # we get the initialisation of this index
                 auxResult=(listOfElements[i]-listOfCouples[i][0])
-
+                #for next index
                 for c in range (i+1,len(listOfCouples)):
+                    # we get the couple
                     couple = listOfCouples[c]
-                    auxResult*=(couple[1]-couple[0]-1)
+                    auxResult*=(couple[1]-couple[0]-1)+1
                 result+=auxResult
             return result
         self.set[name]=newfunction
         self.iterator = self.set[name]([ (couple[1]-1) for couple in listOfCouples ])+1
 
-    def get(self,name,listOfElements):
+    def getVarNumber(self,name,listOfElements):
         return self.set[name](listOfElements)
 
+    def get(self, name):
+        return self.set[name]
 
-test=setOfVariables()
+'''
+test=variablesGenerator()
 
 test.add("m",[(0,4),(0,5),(0,8)])
 test.add("l",[(0,3),(1,5),(0,9)])
@@ -44,3 +70,4 @@ for l in range (1,5):
     print (test.get("x",[l]))
 print(test.iterator)
 
+'''
