994c0dbddf50b8cc5e03ae2c72f176726484706e
Mathilde BOLTENHAGEN <mathilde.boltenhagen@outlook.com>
2019-10-08 13:32:34 +0200

    move example

--- examples/testfile.py
+++ examples/testfile.py
@@ -1,16 +1,16 @@
 
-# last version is not updated for importer, mine is
 import sys
 
 from src.main.conformanceArtefacts import ConformanceArtefacts
 
+# last version is not updated for importer, mine is
 sys.path.append('../pm4py-source')
 from pm4py.objects.petri import importer
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.petrinet import factory as vizu
 
-net, m0, mf = importer.pnml.import_net("./examples/medium/CloseToM8.pnml")
-log = xes_importer.import_log("./examples/medium/CloseToM8.xes")
+net, m0, mf = importer.pnml.import_net("./medium/CloseToM8.pnml")
+log = xes_importer.import_log("./medium/CloseToM8.xes")
 
 
 #vizu.apply(net,m0,mf).view()
