381bde07f555ea3a44a4841dc755cd93fbc1ee34
Mathilde BOLTENHAGEN <mathilde.boltenhagen@outlook.com>
2019-10-09 11:07:01 +0200

    change main dir name

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-    da4py implements state-of-the-art Process Mining methods over SAT encoding. An Ocaml version is Darksider.
-"""
-
-name = "da4py"
-
