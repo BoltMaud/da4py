6e853ce542af468e06f753553336d165b7cdd714
Mathilde BOLTENHAGEN <mathilde.boltenhagen@outlook.com>
2019-10-09 11:03:59 +0200

    add setup.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    da4py implements state-of-the-art Process Mining methods over SAT encoding. An Ocaml version is Darksider.
+"""
+
+name = "da4py"
+
--- examples/testfile.py
+++ examples/testfile.py
@@ -9,13 +9,13 @@ from pm4py.objects.petri import importer
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.petrinet import factory as vizu
 
-net, m0, mf = importer.pnml.import_net("./medium/CloseToM8.pnml")
-log = xes_importer.import_log("./medium/CloseToM8.xes")
+net, m0, mf = importer.pnml.import_net("./examples/medium/CloseToM8.pnml")
+log = xes_importer.import_log("./examples/medium/CloseToM8.xes")
 
 
-#vizu.apply(net,m0,mf).view()
+vizu.apply(net,m0,mf).view()
 
-artefacts= ConformanceArtefacts(6,13)
+artefacts= ConformanceArtefacts(size_of_run=6,max_d=13)
 artefacts.multiAlignment(net,m0,mf,log)
 print(artefacts.getRun())
 print(artefacts.getTracesWithDistances())
